f3f964637c7f7705849ec1179b92f4906fde701c
Julieven <jhv@clin.au.dk>
2022-09-22 11:19:32 +0200

    test filter2

--- tests/testthat/test-filter2.R
+++ tests/testthat/test-filter2.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("filter2 works with dataframe", {
+  df <- filter2(iris, c(TRUE,FALSE))
+  expect_s3_class(df, "data.frame")
+  expect_equal(filter2(iris,iris$Species == "setosa"),
+               dplyr::filter(iris, Species == "setosa"))
 })
